96c4ab73d97c2333e61b2748267c6c468274133c
narcelio <narcelio16@hotmail.com>
2021-12-20 14:04:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React ,{useState,useFor}from 'react'
 import './App.css';
 
 function App() {
+  const [soma, setsoma] = useState()
+  const [n1, setn1] = useState()
+  const [n2, setn2] = useState()
+  const onSubmit =() =>{
+   
+     setsoma(n1+n2)
+    
+  }
+
+  console.log(soma)
+
+
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <div className='calc' >
+        
+          <div className='body'>
+          <center><h4>{process.env.REACT_APP_TITLE}</h4></center>
+            <input type='number' name='n1' value={n1} onChange={e => setn1(e.target.valueAsNumber)} placeholder='Valor '/>
+            <input type='number' name='n2' value={n2} onChange={e => setn2(e.target.valueAsNumber)} placeholder='Valor'/><br/>
+            <div className='result'> 
+            <button type='submit' onClick={onSubmit}>+</button>
+            </div>
+            <div className='soma'>
+              {soma}
+            </div>
+            </div>
+            
+        </div>
+      
+    
   );
 }
 
